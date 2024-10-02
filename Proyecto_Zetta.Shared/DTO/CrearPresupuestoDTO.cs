b6d678a45e4eb104088092108a6f19e3be7b327f
Andrés Zane <andresnicolaszanetta@gmail.com>
2024-10-02 12:42:15 -0300

    Creacion del DTO de Presupuesto.

---
+++ Proyecto_Zetta.Shared/DTO/CrearPresupuestoDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Zetta.Shared.DTO
+{
+     class CrearPresupuestoDTO
+     {
+        [Required(ErrorMessage = "El Estado del presupuesto es obligatorio.")]
+        public string Estado { get; set; }
+
+        [Required(ErrorMessage = "Los insumos del presupuesto son obligatorios.")]
+        public long Insumos { get; set; }
+
+        [Required(ErrorMessage = "La mano de obra del presupuesto es obligatoria.")]
+        public long ManodeObra { get; set; }
+
+        [Required(ErrorMessage = "El Precio Final del presupuesto es obligatorio.")]
+        public long PrecioFinal { get; set; }
+
+     }
+}
